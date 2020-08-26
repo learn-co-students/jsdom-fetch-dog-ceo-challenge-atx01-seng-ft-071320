99a992450e09f057d9f542808280516c3a11145b
lgonzalez2 <lgonzalez_94@hotmail.com>
2020-08-26 11:43:51 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,75 @@
 console.log('%c HI', 'color: firebrick')
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadImages();
+    loadBreeds();
+});
+
+function loadImages() {
+    const imgUrl = "https://dog.ceo/api/breeds/image/random/4"
+    fetch(imgUrl)
+        .then(res => res.json())
+        .then(results => {
+        results.message.forEach(image => addImage(image))
+    });
+}
+
+function addImage(image) {
+    const container = document.getElementById('dog-image-container');
+    const imgElement = document.createElement('img');
+    imgElement.setAttribute('src', image);
+    container.append(imgElement);
+}
+
+function loadBreeds() {
+    const breedUrl = 'https://dog.ceo/api/breeds/list/all'
+    fetch(breedUrl)
+        .then(res => res.json())
+        .then(results => {
+    breeds = Object.keys(results.message);
+    updateBreedList(breeds);
+    addBreedSelectListener();
+    });
+}
+
+function updateBreedList(breeds) {
+    let breedsList = document.getElementById('dog-breeds');
+    removeChildren(breedsList);
+    breeds.forEach(breed => addBreed(breed))
+}
+
+function removeChildren(element) {
+    let child = element.lastElementChild;
+    while (child) {
+      element.removeChild(child);
+      child = element.lastElementChild;
+    }
+}
+
+function addBreed(breed) {
+    let breedsList = document.getElementById('dog-breeds');
+    const item = document.createElement('li');
+    item.innerText = breed;
+    breedsList.append(item);
+    item.addEventListener('click', updateColor);
+}
+
+function updateColor(e) {
+    if (e.target.style.color == 'red') {
+        e.target.style.color = 'black';
+    } else {
+        e.target.style.color = 'red';
+    }
+}
+
+function addBreedSelectListener() {
+    const breedDropDown = document.getElementById('breed-dropdown');
+    breedDropDown.addEventListener('change', function (event) {
+        selectBreedsStartingWith(event.target.value);
+    });
+}
+
+function selectBreedsStartingWith(letter) {
+    updateBreedList(breeds.filter(breed => breed.startsWith(letter)));
+}
